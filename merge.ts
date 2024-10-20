2858c6eac871d9bf2819aaa77a5673a755ff177b
Marvin Cordero Leyton <marvin_cordley@live.com>
2024-10-20 17:50:44 -0400

    feature(common): documented merged branches in merge.ts

---
+++ merge.ts
@@ -0,0 +1,10 @@
+// List of merged branches in LAB04
+// Merged branches:
+
+// 1. Merged feature/LAB04-01 into master
+//    Description: This merge included the base files and answers to questions 1-4.
+//    PR-001 merged successfully.
+
+// 2. Merged feature/Question-5 into master
+//    Description: This merge included the solution to Question 5.
+//    PR-002 merged successfully.
